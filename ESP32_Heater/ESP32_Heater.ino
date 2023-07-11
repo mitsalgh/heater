612a96fcd6d0b6f2939acec328f026955364cacd
witaradya <witaradya.a.dharma@polytron.co.id>
2023-07-11 14:48:59 +0700

    update esp32 heater

--- ESP32_Heater/ESP32_Heater.ino
+++ ESP32_Heater/ESP32_Heater.ino
@@ -8,11 +8,13 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
+#include <WiFiManager.h>
 
 #define SSID     "rajaaa"
 #define PASSWORD "18091998"
 
 BlynkTimer timer;
+WiFiManager wifiManager;
 
 float suhuValue1 = 0.0, suhuValue2 = 0.0, suhuValue3 = 0.0, suhuValue4 = 0.0;
 byte dataInput[8];
@@ -28,32 +30,63 @@ void myTimerEvent()
 void setup() {
   Serial.begin(115200);
   Serial2.begin(9600);
+  pinMode(2, OUTPUT);
 
-  Blynk.begin(BLYNK_AUTH_TOKEN, SSID, PASSWORD);
+  WiFi_WifiManager();
+
+  Blynk.config(BLYNK_AUTH_TOKEN);
+  Blynk.run();
+  // Blynk.begin(BLYNK_AUTH_TOKEN, SSID, PASSWORD);
   timer.setInterval(1000L, myTimerEvent);
 }
 
 void loop() {
-  Blynk.run();
-  timer.run();
-  
-  if(Serial2.available() > 7){
-    // for(int a = 0; a < 8; a++){
-    //   dataInput[a] = Serial2.read();
-    //   Serial.print(dataInput[a]);
-    //   Serial.print("\t");
-    // }
-    // Serial.println();
-    suhuValue1 = ((dataInput[0] << 8) + dataInput[1]) / 100.0;
-    suhuValue2 = ((dataInput[2] << 8) + dataInput[3]) / 100.0;
-    suhuValue3 = ((dataInput[4] << 8) + dataInput[5]) / 100.0;
-    suhuValue4 = ((dataInput[6] << 8) + dataInput[7]) / 100.0;
-    // Serial.print(suhuValue1);
-    // Serial.print("\t");
-    // Serial.print(suhuValue2);
-    // Serial.print("\t");
-    // Serial.print(suhuValue3);
-    // Serial.print("\t");
-    // Serial.println(suhuValue4);
+  // WiFi_ManagerReconnect();
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("WiFi Manager : Reconnect WiFi Manager");
+    // wifiManager.resetSettings();
+    wifiManager.autoConnect("HEATER-KU");
+    Serial.println("WiFi Manager : Reconnect WiFi Manager Success");
+  }
+  else{
+    Blynk.run();
+    timer.run();
+    
+    if(Serial2.available() > 7){
+//      Serial.println(Serial2.read());
+       for(int a = 0; a < 8; a++){
+         dataInput[a] = Serial2.read();
+         Serial.print(dataInput[a]);
+         Serial.print("\t");
+       }
+       Serial.println();
+      suhuValue1 = ((dataInput[0] << 8) + dataInput[1]) / 100.0;
+      suhuValue2 = ((dataInput[2] << 8) + dataInput[3]) / 100.0;
+      suhuValue3 = ((dataInput[4] << 8) + dataInput[5]) / 100.0;
+      suhuValue4 = ((dataInput[6] << 8) + dataInput[7]) / 100.0;
+      // Serial.print(suhuValue1);
+      // Serial.print("\t");
+      // Serial.print(suhuValue2);
+      // Serial.print("\t");
+      // Serial.print(suhuValue3);
+      // Serial.print("\t");
+      // Serial.println(suhuValue4);
+    }
+  }
+}
+
+void WiFi_WifiManager(){
+  Serial.println("WiFi : Init WiFi Manager");
+  // wifiManager.resetSettings();
+  wifiManager.autoConnect("HEATER-KU");
+  Serial.println("WiFi : WiFi Manager Connected");
+}
+
+void WiFi_ManagerReconnect(){
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("WiFi Manager : Reconnect WiFi Manager");
+    // wifiManager.resetSettings();
+    wifiManager.autoConnect("HEATER-KU");
+    Serial.println("WiFi Manager : Reconnect WiFi Manager Success");
   }
 }
