05750e13651377e9d6b2ab6b54e30a6a1a590aff
witaradya <witaradya.a.dharma@polytron.co.id>
2023-07-15 16:47:15 +0700

    Fix Komunikasi UART

--- ESP32_Heater/ESP32_Heater.ino
+++ ESP32_Heater/ESP32_Heater.ino
@@ -17,7 +17,9 @@ BlynkTimer timer;
 WiFiManager wifiManager;
 
 float suhuValue1 = 0.0, suhuValue2 = 0.0, suhuValue3 = 0.0, suhuValue4 = 0.0;
-byte dataInput[8];
+byte dataInput[10];
+byte countData = 0;
+bool header = false;
 
 void myTimerEvent()
 {
@@ -52,25 +54,57 @@ void loop() {
     Blynk.run();
     timer.run();
     
-    if(Serial2.available() > 7){
+    if(Serial2.available() > 0){
+      byte data = Serial2.read();
+      Serial.print(countData);
+      Serial.print(" ");
+      Serial.println(data);
+      if(countData == 0){
+        if(data == 0xAA) {
+          dataInput[0] = data;
+          countData++;
+        }
+        else countData = 0;
+      }
+      else if(countData == 1) {
+        if(data == 0x10) {
+          dataInput[1] = data;
+          countData++;
+          return;
+        }
+        else countData = 0;
+      }
+
+      if(dataInput[0] == 0xAA && dataInput[1] == 0x10 && countData > 1){
+        dataInput[countData] = data;
+        countData++;
+
+        if(countData >= 10){
+          countData = 0;
+          suhuValue1 = ((dataInput[2] << 8) + dataInput[3]) / 100.0;
+          suhuValue2 = ((dataInput[4] << 8) + dataInput[5]) / 100.0;
+          suhuValue3 = ((dataInput[6] << 8) + dataInput[7]) / 100.0;
+          suhuValue4 = ((dataInput[8] << 8) + dataInput[9]) / 100.0;
+          Serial.print(suhuValue1);
+          Serial.print("\t");
+          Serial.print(suhuValue2);
+          Serial.print("\t");
+          Serial.print(suhuValue3);
+          Serial.print("\t");
+          Serial.println(suhuValue4);
+        }
+      }
+
+      
 //      Serial.println(Serial2.read());
-       for(int a = 0; a < 8; a++){
-         dataInput[a] = Serial2.read();
-         Serial.print(dataInput[a]);
-         Serial.print("\t");
-       }
-       Serial.println();
-      suhuValue1 = ((dataInput[0] << 8) + dataInput[1]) / 100.0;
-      suhuValue2 = ((dataInput[2] << 8) + dataInput[3]) / 100.0;
-      suhuValue3 = ((dataInput[4] << 8) + dataInput[5]) / 100.0;
-      suhuValue4 = ((dataInput[6] << 8) + dataInput[7]) / 100.0;
-      // Serial.print(suhuValue1);
-      // Serial.print("\t");
-      // Serial.print(suhuValue2);
-      // Serial.print("\t");
-      // Serial.print(suhuValue3);
-      // Serial.print("\t");
-      // Serial.println(suhuValue4);
+      //  for(int a = 0; a < 8; a++){
+      //    dataInput[a] = Serial2.read();
+      //    Serial.print(dataInput[a]);
+      //    Serial.print("\t");
+      //  }
+      //  Serial.println();
+      
+      // 
     }
   }
 }
